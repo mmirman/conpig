1280ca84e2c016557d2ab229f8f8b4959bdf7a4e
Matthew Mirman <titaniumeyes@aol.com>
2013-08-13 18:00:49 -0400

    even simpler. one process now.

--- conpig.py
+++ conpig.py
@@ -2,8 +2,6 @@ import sys
 
 from greenlet import greenlet
 import signal
-import os
-from multiprocessing import Process
 from collections import deque
 import time
 
@@ -18,7 +16,7 @@ def next(argA, argB):
     t = threads.popleft()
     
     if t.dead:
-        return next(argA,argB)
+        return next(argA, argB)
 
     threads.append(t)
     t.switch()
@@ -29,32 +27,18 @@ def forkIO(method, *args, **kr):
     threads.append(greenlet(methodp))
 
 def runMain(main):
-    def mainProcess():
-        forkIO(main)
-        signal.signal(signal.SIGCHLD, next)
-        # attempt to reap every 4 seconds
-        while len(threads) > 0:
-            time.sleep(4) 
+    forkIO(main)
+    signal.signal(signal.SIGALRM, next)
+    signal.setitimer(signal.ITIMER_REAL, 0.000001, 0.000001)
+    next()
 
-    process = Process(target = mainProcess)
-    
-    try: 
-        process.start()
-        pid = process.pid
-    
-        while process.is_alive():
-            time.sleep(0.000001)
-            os.kill(pid, signal.SIGCHLD)
-            
-    finally:
-        process.terminate()
-
+    while len(threads) > 0:
+        # I'm told pause sucks
+        time.sleep(4)
 
 def test(arg):
     for i in range(0,4000):
         print arg
-
-
 
 def main():
     forkIO(test, "X")    
